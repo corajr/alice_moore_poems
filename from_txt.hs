160559b55b5090b951d930049e1a3f74ea95d367
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2017-04-22 10:32:49 -0400

    add labels

--- from_txt.hs
+++ from_txt.hs
@@ -1,14 +1,32 @@
 #!/usr/bin/env stack
--- stack --resolver lts-8.11 --install-ghc runghc
+-- stack --resolver lts-8.11 --install-ghc runghc --package text
+{-# LANGUAGE OverloadedStrings #-}
 
 import System.Environment (getArgs)
+import Data.Text (Text)
+import Data.Monoid ((<>))
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Data.List (maximumBy)
 import Data.Function (on)
 
 main :: IO ()
-main = getArgs >>= mapM_ process
+main = getArgs >>= mapM_ doAddLabel
 
-process :: String -> IO ()
+
+doAddLabel :: FilePath -> IO ()
+doAddLabel inFile = do
+  let outName = takeWhile (/= '.') . drop 6 $ inFile
+  input <- T.readFile inFile
+  let out = addLabel (T.pack outName) (T.lines input)
+  T.writeFile inFile (T.unlines out)
+
+addLabel :: Text -> [Text] -> [Text]
+addLabel name input = begin ++ ["\\label{ch:" <> name <> "}"] ++ end
+  where
+    (begin, end) = splitAt 1 input
+
+process :: FilePath -> IO ()
 process x = do
   let outFile = "poems/" ++ (iterate init . drop 1 . dropWhile (/= '/') $ x) !! 4 ++ ".tex"
   input <- readFile x
